7cb53205ec5b321bda88e267238c5f1090ab1c3b
unknown <shafaes@code.edu.az>
2022-09-08 15:23:42 +0400

    color change to image

--- js/app.js
+++ js/app.js
@@ -3,19 +3,21 @@
  console.log(hour);
  if (hour >= 6 && hour <= 12) {
  alert('Sabahin Xeyir Baki....')
- document.body.style.backgroundColor="yellow"
+ document.body.style.backgroundImage="url('https://metbuat.az/images/axsam/images_d/306895_3.jpg')"
+ 
  }
  else if(hour>12 && hour <=18) {
      alert('Gunortan Xeyir Baki...')
-     document.body.style.backgroundColor = "orange"
+      document.body.style.backgroundImage="url('https://toplum.tv/images/2020-04/1586169886_trend_baku_city_spring_270415_37.jpg')"
  }
  else if(hour> 18 && hour <=24) {
      alert('Axsamin Xeyir Baki...')
-     document.body.style.backgroundColor = "blue"
+     document.body.style.backgroundImage="url('https://news24.az/uploads/posts/2019-08/1567060512_baki.jpg')"
  }
  else if(hour> 0 && hour <6) {
      alert('Şirin yuxular Bakı...')
-     document.body.style.backgroundColor = "dark"
+     document.body.style.backgroundImage="url('https://cdn.oxu.az/uploads/W1siZiIsIjIwMTUvMDIvMDUvMTMvMDAvMTUvNDQ1L2Jha3UyLmpwZyJdLFsicCIsImVuY29kZSIsImpwZyIsIi1xdWFsaXR5IDgwIl0sWyJwIiwidGh1bWIiLCI2MjB4NDEzIyJdXQ?sha=ab237c1d3d06ca4d')"
+
  }
 
        
